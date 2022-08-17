23e0f7a9cf6e1da6eb7ff63039638d2abbcc1d36
nsetradingsalem <nsetradingsalem@gmail.com>
2022-08-17 08:43:34 +0530

    first commit

--- myproject/celery.py
+++ myproject/celery.py
@@ -28,12 +28,3 @@ app.conf.broker_url = BASE_REDIS_URL
 # this allows you to schedule items in the Django admin.
 app.conf.beat_scheduler = 'django_celery_beat.schedulers.DatabaseScheduler'
 
-
-
-import os
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_celery.settings")
-app = Celery("django_celery")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
