fb3ec08cb58633dfbee43a49b7cca38c6de5df3c
nsetradingsalem <nsetradingsalem@gmail.com>
2022-08-30 12:57:37 +0530

    30-Aug update 4

--- myproject/settings.py
+++ myproject/settings.py
@@ -167,9 +167,11 @@ USE_L10N = True
 
 USE_TZ = False
 
+CELERY_BROKER_URL = 'amqp://localhost'
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
 # BROKER_URL = 'django://'
-CELERY_BROKER_URL = 'redis://:p1b6b84e67facd107957e878035c0fc2ad3da5f6d4ca0c138e7bac0f4a3bfb055@ec2-54-152-181-10.compute-1.amazonaws.com:27179'
-CELERY_RESULT_BACKEND = 'redis://:p1b6b84e67facd107957e878035c0fc2ad3da5f6d4ca0c138e7bac0f4a3bfb055@ec2-54-152-181-10.compute-1.amazonaws.com:27179'
+# CELERY_BROKER_URL = 'redis://:p1b6b84e67facd107957e878035c0fc2ad3da5f6d4ca0c138e7bac0f4a3bfb055@ec2-54-152-181-10.compute-1.amazonaws.com:27179'
+# CELERY_RESULT_BACKEND = 'redis://:p1b6b84e67facd107957e878035c0fc2ad3da5f6d4ca0c138e7bac0f4a3bfb055@ec2-54-152-181-10.compute-1.amazonaws.com:27179'
 # Static files (CSS, JavaScript, Images)
 # Celery Configuration Options
 # CELERY_TIMEZONE = "Asia/kol"
