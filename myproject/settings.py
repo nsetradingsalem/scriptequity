577db7bb199b08d1e1298323d9abdaa758d44b97
nsetradingsalem <nsetradingsalem@gmail.com>
2022-08-30 13:06:46 +0530

    30-Aug update 4

--- myproject/settings.py
+++ myproject/settings.py
@@ -167,8 +167,8 @@ USE_L10N = True
 
 USE_TZ = False
 
-CELERY_BROKER_URL = 'amqp://localhost'
-CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+# CELERY_BROKER_URL = 'amqp://localhost'
+CELERY_BROKER_URL = 'redis://localhost:6379'
 # BROKER_URL = 'django://'
 # CELERY_BROKER_URL = 'redis://:p1b6b84e67facd107957e878035c0fc2ad3da5f6d4ca0c138e7bac0f4a3bfb055@ec2-54-152-181-10.compute-1.amazonaws.com:27179'
 # CELERY_RESULT_BACKEND = 'redis://:p1b6b84e67facd107957e878035c0fc2ad3da5f6d4ca0c138e7bac0f4a3bfb055@ec2-54-152-181-10.compute-1.amazonaws.com:27179'
